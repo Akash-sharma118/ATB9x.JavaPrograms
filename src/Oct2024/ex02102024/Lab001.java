5904446301a48a567c43a066d6dd95d32c48f633
Akash-sharma118 <akash9909669@gmail.com>
2024-10-09 22:29:26 +0400

    All File Added

---
+++ src/Oct2024/ex02102024/Lab001.java
@@ -0,0 +1,13 @@
+package Oct2024.ex02102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+
+    }
+
+
+
+}
---
+++ src/Oct2024/ex03102024/Lab003.java
@@ -0,0 +1,15 @@
+package Oct2024.ex03102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Simple Claculator");
+        System.out.println("Addition");
+        System.out.println("100+40=140");
+        System.out.println("Multiplication");
+        System.out.println("13*4=52");
+        System.out.println("Subtraction");
+        System.out.println("13-4=9");
+        System.out.println("Division");
+        System.out.println("12/6=2");;
+    }
+}
